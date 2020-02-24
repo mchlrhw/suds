72817cce2c9b39dce966385f478efabcb098aa22
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-02-24 13:19:55 +0000

    Implement colourised output
    This is really hacky. Hopefully there's a cleaner way to insert the
    formatting that can be unified with the Display impl.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use std::{collections::HashSet, convert::TryInto, fmt, num::ParseIntError, str::FromStr};
+use std::{
+    collections::HashSet,
+    convert::TryInto,
+    fmt,
+    io::{stdout, Write},
+    num::ParseIntError,
+    str::FromStr,
+};
 
 use clap::Clap;
+use crossterm::{
+    queue,
+    style::{style, Attribute, Color, PrintStyledContent, StyledContent},
+};
+use fehler::throws;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 enum Value {
@@ -226,6 +238,93 @@ impl fmt::Display for Grid {
     }
 }
 
+impl Grid {
+    pub fn to_styled(&self) -> Vec<StyledContent<String>> {
+        let mut val = vec![];
+        val.push(style(" _____________________________\n".to_string()));
+        for y in 0..=8 {
+            val.push(style('|'.to_string()));
+            for x in 0..=8 {
+                let space = self.get(x, y);
+                let styled = match space {
+                    Space::Occupied(v) => match v {
+                        Value::One => style(format!(" {} ", v))
+                            .with(Color::Rgb { r: 255, g: 0, b: 0 })
+                            .attribute(Attribute::Bold),
+                        Value::Two => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 255,
+                                g: 120,
+                                b: 0,
+                            })
+                            .attribute(Attribute::Bold),
+                        Value::Three => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 255,
+                                g: 255,
+                                b: 0,
+                            })
+                            .attribute(Attribute::Bold),
+                        Value::Four => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 120,
+                                g: 255,
+                                b: 0,
+                            })
+                            .attribute(Attribute::Bold),
+                        Value::Five => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 0,
+                                g: 180,
+                                b: 80,
+                            })
+                            .attribute(Attribute::Bold),
+                        Value::Six => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 0,
+                                g: 150,
+                                b: 255,
+                            })
+                            .attribute(Attribute::Bold),
+                        Value::Seven => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 60,
+                                g: 80,
+                                b: 220,
+                            })
+                            .attribute(Attribute::Bold),
+                        Value::Eight => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 140,
+                                g: 40,
+                                b: 255,
+                            })
+                            .attribute(Attribute::Bold),
+                        Value::Nine => style(format!(" {} ", v))
+                            .with(Color::Rgb {
+                                r: 240,
+                                g: 20,
+                                b: 255,
+                            })
+                            .attribute(Attribute::Bold),
+                    },
+                    Space::Empty => style(space.to_string()).with(Color::Grey),
+                };
+                val.push(styled);
+                if (x + 1) % 3 == 0 {
+                    val.push(style('|'.to_string()));
+                }
+            }
+            val.push(style('\n'.to_string()));
+            if (y + 1) % 3 == 0 {
+                val.push(style("|_____________________________|\n".to_string()));
+            }
+        }
+
+        val
+    }
+}
+
 impl From<Vec<u8>> for Grid {
     fn from(numbers: Vec<u8>) -> Self {
         let mut spaces = [Space::Empty; 81];
@@ -271,13 +370,19 @@ enum Subcommand {
 #[derive(Clap)]
 struct Generate {}
 
+#[throws(crossterm::ErrorKind)]
 fn main() {
+    let mut stdout = stdout();
     let opts = Opts::parse();
+
     match opts.subcmd {
         Subcommand::Generate(_) => {
             let grid = Grid::empty();
             let grid = grid.solve().expect("Unsolvable");
-            println!("{}", grid);
+            for s in grid.to_styled() {
+                queue!(stdout, PrintStyledContent(s))?;
+            }
+            stdout.flush()?;
         }
     }
 }
