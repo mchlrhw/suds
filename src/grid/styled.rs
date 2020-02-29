b4ed08e100ab813c1db2410a1e23a944289703a2
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-02-29 11:58:31 +0000

    Parameterise Grid::to_styled

--- src/grid/styled.rs
+++ src/grid/styled.rs
@@ -4,13 +4,30 @@ use crate::{space::Space, value::Value};
 
 use super::Grid;
 
+const TOP_LEFT: char = ',';
+const TOP_RIGHT: char = ',';
+const BOTTOM_LEFT: char = '\'';
+const BOTTOM_RIGHT: char = '\'';
+const OUTSIDE_ACROSS: char = '-';
+const INSIDE_ACROSS: char = '-';
+const OUTSIDE_DOWN: char = '|';
+const INSIDE_DOWN: char = '|';
+const INSIDE_INTERSECTION: char = '+';
+
 impl Grid {
     pub fn to_styled(&self) -> Vec<StyledContent<String>> {
         let mut val = vec![];
-        val.push(style(",-----------------------------,\n".to_string()));
-        for y in 0..=8 {
-            val.push(style('|'.to_string()));
-            for x in 0..=8 {
+
+        val.push(style(TOP_LEFT.to_string()));
+        for _ in 0..29 {
+            val.push(style(OUTSIDE_ACROSS.to_string()));
+        }
+        val.push(style(TOP_RIGHT.to_string()));
+        val.push(style("\n".to_string()));
+
+        for y in 0..9 {
+            val.push(style(OUTSIDE_DOWN.to_string()));
+            for x in 0..9{
                 let space = self.get(x, y);
                 let styled = match space {
                     Space::Occupied(v) => match v {
@@ -77,19 +94,40 @@ impl Grid {
                     Space::Empty => style(format!(" {} ", space)).with(Color::Grey),
                 };
                 val.push(styled);
+                if x == 8 {
+                    break;
+                }
                 if (x + 1) % 3 == 0 {
-                    val.push(style('|'.to_string()));
+                    val.push(style(INSIDE_DOWN.to_string()));
                 }
             }
+            val.push(style(OUTSIDE_DOWN.to_string()));
             val.push(style('\n'.to_string()));
             if y == 8 {
                 break;
             }
             if (y + 1) % 3 == 0 {
-                val.push(style("|---------+---------+---------|\n".to_string()));
+                val.push(style(OUTSIDE_DOWN.to_string()));
+                for _ in 0..2 {
+                    for _ in 0..9 {
+                        val.push(style(INSIDE_ACROSS.to_string()));
+                    }
+                    val.push(style(INSIDE_INTERSECTION.to_string()));
+                }
+                for _ in 0..9 {
+                    val.push(style(INSIDE_ACROSS.to_string()));
+                }
+                val.push(style(OUTSIDE_DOWN.to_string()));
+                val.push(style("\n".to_string()));
             }
         }
-        val.push(style("'-----------------------------'\n".to_string()));
+
+        val.push(style(BOTTOM_LEFT.to_string()));
+        for _ in 0..29 {
+            val.push(style(OUTSIDE_ACROSS.to_string()));
+        }
+        val.push(style(BOTTOM_RIGHT.to_string()));
+        val.push(style("\n".to_string()));
 
         val
     }
