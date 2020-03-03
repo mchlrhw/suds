23eff68faf573535c7937dcb7757bdfde59d8200
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-03-03 19:52:42 +0000

    Move colours into Value

--- src/grid/styled.rs
+++ src/grid/styled.rs
@@ -1,6 +1,6 @@
 use crossterm::style::{style, Attribute, Color, StyledContent};
 
-use crate::{space::Space, value::Value};
+use crate::space::Space;
 
 use super::Grid;
 
@@ -30,67 +30,9 @@ impl Grid {
             for x in 0..9 {
                 let space = self.get(x, y);
                 let styled = match space {
-                    Space::Occupied(v) => match v {
-                        Value::One => style(format!(" {} ", v))
-                            .with(Color::Rgb { r: 255, g: 0, b: 0 })
-                            .attribute(Attribute::Bold),
-                        Value::Two => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 255,
-                                g: 120,
-                                b: 0,
-                            })
-                            .attribute(Attribute::Bold),
-                        Value::Three => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 255,
-                                g: 255,
-                                b: 0,
-                            })
-                            .attribute(Attribute::Bold),
-                        Value::Four => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 120,
-                                g: 255,
-                                b: 0,
-                            })
-                            .attribute(Attribute::Bold),
-                        Value::Five => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 0,
-                                g: 180,
-                                b: 80,
-                            })
-                            .attribute(Attribute::Bold),
-                        Value::Six => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 0,
-                                g: 150,
-                                b: 255,
-                            })
-                            .attribute(Attribute::Bold),
-                        Value::Seven => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 60,
-                                g: 80,
-                                b: 220,
-                            })
-                            .attribute(Attribute::Bold),
-                        Value::Eight => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 140,
-                                g: 40,
-                                b: 255,
-                            })
-                            .attribute(Attribute::Bold),
-                        Value::Nine => style(format!(" {} ", v))
-                            .with(Color::Rgb {
-                                r: 240,
-                                g: 20,
-                                b: 255,
-                            })
-                            .attribute(Attribute::Bold),
-                    },
+                    Space::Occupied(v) => style(format!(" {} ", v))
+                        .with(v.color())
+                        .attribute(Attribute::Bold),
                     Space::Empty => style(format!(" {} ", space)).with(Color::Grey),
                 };
                 val.push(styled);
--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashSet, fmt};
 
+use crossterm::style::Color;
 use lazy_static::lazy_static;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
@@ -19,6 +20,54 @@ pub enum Value {
     Seven,
     Eight,
     Nine,
+}
+
+impl Value {
+    pub fn color(&self) -> Color {
+        match self {
+            Self::One => Color::Rgb { r: 255, g: 0, b: 0 },
+            Self::Two => Color::Rgb {
+                r: 255,
+                g: 120,
+                b: 0,
+            },
+            Self::Three => Color::Rgb {
+                r: 255,
+                g: 255,
+                b: 0,
+            },
+            Self::Four => Color::Rgb {
+                r: 120,
+                g: 255,
+                b: 0,
+            },
+            Self::Five => Color::Rgb {
+                r: 0,
+                g: 180,
+                b: 80,
+            },
+            Self::Six => Color::Rgb {
+                r: 0,
+                g: 150,
+                b: 255,
+            },
+            Self::Seven => Color::Rgb {
+                r: 60,
+                g: 80,
+                b: 220,
+            },
+            Self::Eight => Color::Rgb {
+                r: 140,
+                g: 40,
+                b: 255,
+            },
+            Self::Nine => Color::Rgb {
+                r: 240,
+                g: 20,
+                b: 255,
+            },
+        }
+    }
 }
 
 impl fmt::Display for Value {
